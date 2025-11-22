e1cf1f29aafb2fdd48d9db93a3d15d15841eb003
Anett Yomali Vera Carbajal <132587180+AnettVera@users.noreply.github.com>
2025-11-21 20:00:52 -0600

    Remove AppDelegate.swift file

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,1 @@
-import Flutter
-import UIKit
-import GoogleMaps
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    // Provide Google Maps API Key
-    GMSServices.provideAPIKey("AIzaSyBGunly0ioS2Ex6hzuEUkWNR8E_ALIqS84")
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-}
